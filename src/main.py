d527b89766fae41ea967230590b7e94fd712fedc
talkapi <tal.kapitolnik@descope.com>
2025-09-01 14:25:49 +0300

    Add MD5 support
    Add MD5 support

--- src/main.py
+++ src/main.py
@@ -412,6 +412,12 @@ class KeycloakMigrationTool:
                             "memory": int(cred_data.get("additionalParameters", {}).get("memory", ["7168"])[0]),
                             "threads": int(cred_data.get("additionalParameters", {}).get("parallelism", ["1"])[0])
                         }
+                    },
+                elif alg == "MD5":
+                    return {
+                        "md5": {
+                            "hash": secret_data.get("value", "")
+                        }
                     }
                 else:
                     logging.warning(f"Unsupported password algorithm: {alg}")
