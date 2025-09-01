9c4172f1aa8b418fe267979873c86246cd8c1020
Tal Kapitolnik <45332505+talkapi@users.noreply.github.com>
2025-09-01 16:01:14 +0300

    Add MD5 support to the Keycloak migration script (#4)
    * Add MD5 support
    
    Add MD5 support
    
    * Update main.py

--- src/main.py
+++ src/main.py
@@ -413,6 +413,12 @@ class KeycloakMigrationTool:
                             "threads": int(cred_data.get("additionalParameters", {}).get("parallelism", ["1"])[0])
                         }
                     }
+                elif alg == "MD5":
+                    return {
+                        "md5": {
+                            "hash": secret_data.get("value", "")
+                        }
+                    }
                 else:
                     logging.warning(f"Unsupported password algorithm: {alg}")
         return None
